39a3de6fa9b3a87f61e3b984d08618496b27e3ef
cookieroz <cookieroz@gmail.com>
2013-03-16 17:40:03 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BravoRentals::Application.config.secret_token = '9cc4acc466625f0a5fcc48598bbc0863edd9d80f15085b8681f66fb09bc9d86c09aa97327136cc9e92175a95cf27c6631d821f89e958dde2f8be73f2b58c7125'
