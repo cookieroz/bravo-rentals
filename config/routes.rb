b16bd50c6d0e0fa7d8ed76e1cae860fa69483362
cookieroz <cookieroz@gmail.com>
2013-03-17 20:02:23 -0700

    added owners and admin pages

--- config/routes.rb
+++ config/routes.rb
@@ -14,14 +14,14 @@ BravoRentals::Application.routes.draw do
     # collection { post :search, to: 'villas#index' }
   end
 
-  get "static_pages/about"
-
   get 'search', to: 'static_pages#search', as: 'search'
   get 'contact', to: 'static_pages#contact', as: 'contact'
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   match '/admin', to: 'static_pages#admin'
+  get 'owners', to: 'static_pages#owners', as: 'owners'
+  get 'about', to: 'static_pages#about', as: 'about'
 
   resources :photos
 
